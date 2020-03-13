d77f9330bb24a0d20ea09081e669dffc80e21ddf
PedroSLobo <ps.lobo91@gmail.com>
2020-03-13 18:11:35 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -85,9 +85,10 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
-		board.PlacePiece(p, target);
+		board.placePiece(p, target);
 		
 		if (capturedPiece != null) {
 			piecesOnTheBoard.remove(capturedPiece);
@@ -98,11 +99,12 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
-		board.PlacePiece(p, source);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
+		board.placePiece(p, source);
 		
 		if (capturedPiece != null) {
-			board.PlacePiece(capturedPiece, target);
+			board.placePiece(capturedPiece, target);
 			capturedPieces.remove(capturedPiece);
 			piecesOnTheBoard.add(capturedPiece);
 		}
@@ -183,7 +185,7 @@ public class ChessMatch {
 	}	
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
-		board.PlacePiece(piece, new ChessPosition(column, row).toPosition());
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 		piecesOnTheBoard.add(piece);
 	}
 	
